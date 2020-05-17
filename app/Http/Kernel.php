cfac012e92573e2a9fcd37cc80edf35edf81d63d
BlackRider666 <loverider2012@gmail.com>
2020-05-17 21:35:30 +0300

    cors update latest

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        Cors::class,
+        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
@@ -45,6 +45,7 @@ class Kernel extends HttpKernel
             EnsureFrontendRequestsAreStateful::class,
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Fruitcake\Cors\HandleCors::class,
         ],
     ];
 
