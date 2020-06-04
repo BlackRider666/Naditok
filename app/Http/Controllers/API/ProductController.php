05abd6ce7b35904c6e198e09106d330fd4da4e1b
BlackRider666 <loverider2012@gmail.com>
2020-06-04 21:39:39 +0300

    update product-group

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\ProductGroup\Product\Product;
+use App\ProductGroup\ProductGroup;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return response()->json(Product::all(),200);
+        return response()->json(ProductGroup::all(),200);
     }
 }
