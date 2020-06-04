9c3ca2b7b53a21142f124366d40f58af397d0a2f
BlackRider666 <loverider2012@gmail.com>
2020-06-04 22:34:11 +0300

    update photos show

--- resources/views/components/grid.blade.php
+++ resources/views/components/grid.blade.php
@@ -2,7 +2,7 @@
     <div class="frame-wrap">
         <div class="card-deck">
             @foreach($items as $item)
-                <div class="card border m-auto m-lg-0" style="max-width: 18rem;">
+                <div class="card" style="max-width: 18rem;">
                     <img src="{{$item->thumb_url}}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <form class="col" action="{{route('admin.'.$name.'.destroy',$item->id)}}" method="POST">
