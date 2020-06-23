539e21723d8229ec6975bb35a8b95ac4e697a2e6
BlackRider666 <loverider2012@gmail.com>
2020-06-23 20:31:53 +0300

    update discount show

--- resources/views/components/inputs/date.blade.php
+++ resources/views/components/inputs/date.blade.php
@@ -1,1 +1,1 @@
-<input type="date" class="form-control {{$errors->has($name) ? 'is-invalid':''}}" id="{{$name}}" name="{{$name}}" value="{{$value?:now()->format('Y-m-d')}}">
+<input type="date" class="form-control {{$errors->has($name) ? 'is-invalid':''}}" id="{{$name}}" name="{{$name}}" value="{{$value->format('Y-m-d')?:now()->format('Y-m-d')}}">
