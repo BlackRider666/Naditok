7290a850c666fc37c75adc3ee3732297778f3bb7
BlackRider666 <loverider2012@gmail.com>
2020-06-28 20:52:51 +0300

    update socialite auth v3

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -249,6 +249,6 @@ class AuthController extends Controller
                 'avatar' => (new StorageManager())->savePictureFromUrl($soc->avatar),
             ]);
         }
-        return redirect('http://localhost:3000',['access _token' => $user->createToken('clinic')->plainTextToken]);
+        return redirect('http://localhost:3000?access_token='.$user->createToken('clinic')->plainTextToken);
     }
 }
