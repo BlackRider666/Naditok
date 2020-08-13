75c554953ec3b52396524e177a2350d565bfcf33
BlackRider666 <loverider2012@gmail.com>
2020-08-13 18:05:46 +0300

    update select

--- resources/views/components/inputs/select.blade.php
+++ resources/views/components/inputs/select.blade.php
@@ -1,6 +1,6 @@
 <select class="form-control {{$errors->has($name) ? 'is-invalid':''}}" id="{{$name}}" name="{{$name}}" >
     <option value="" selected disabled hidden>Choose here</option>
     @foreach($items as $item)
-        <option value="{{$item['id']}}" {{$value === $item['id']?'selected':''}}>{{$item['title']?:$item['title_ru']}}</option>
+        <option value="{{$item['id']}}" {{$value === $item['id']?'selected':''}}>{{array_key_exists('title',$item)?$item['title']:$item['title_ru']}}</option>
     @endforeach
 </select>
