0dab903a1fec4955a792f2b4c7a1be97bb71838f
BlackRider666 <loverider2012@gmail.com>
2020-08-16 15:04:59 +0300

    update import seeder

--- database/seeds/ImportTorgSoftSeeder.php
+++ database/seeds/ImportTorgSoftSeeder.php
@@ -32,5 +32,49 @@ class ImportTorgSoftSeeder extends Seeder
             'cat_id'  => 36,
             'out_id'    =>  43,
         ]);
+        CategoryImport::create([
+            'cat_id'  => 10,
+            'out_id'    =>  11,
+        ]);
+        CategoryImport::create([
+            'cat_id'  => 37,
+            'out_id'    =>  1102,
+        ]);
+        CategoryImport::create([
+            'cat_id'  => 38,
+            'out_id'    =>  1101,
+        ]);
+        CategoryImport::create([
+            'cat_id'  => 36,
+            'out_id'    =>  43,
+        ]);
+        CategoryImport::create([
+            'cat_id'  => 11,
+            'out_id'    =>  8803,
+        ]);
+        CategoryImport::create([
+            'cat_id'  => 11,
+            'out_id'    =>  8805,
+        ]);
+        CategoryImport::create([
+            'cat_id'  => 36,
+            'out_id'    =>  43,
+        ]);
+        CategoryImport::create([
+            'cat_id'  => 45,
+            'out_id'    =>  3304,
+        ]);
+        CategoryImport::create([
+            'cat_id'  => 46,
+            'out_id'    =>  3301,
+        ]);
+        CategoryImport::create([
+            'cat_id'  => 51,
+            'out_id'    =>  4806,
+        ]);
+        CategoryImport::create([
+            'cat_id'  => 52,
+            'out_id'    =>  29,
+        ]);
     }
 }
